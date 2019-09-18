e48e6714022413dc184bdde1f943426f47c5acad
j19950805 <j19950805@gmail.com>
2019-09-18 21:15:11 +0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,17 @@
  */
 public class LeapYear {
 
+    /** Check if a given year is a leap year.  */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+	} else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+	} else {
+	    return false;
+        }
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
