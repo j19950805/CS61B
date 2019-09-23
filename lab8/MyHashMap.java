09efb7412e8c42eee028d9c183639ffd6b4870d4
j19950805 <j19950805@gmail.com>
2019-09-23 12:22:24 +0800

    Using ULLMap to implement MyHashMap

---
+++ lab8/MyHashMap.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MyHashMap<K, V> implements Map61B<K, V> {
+    private double loadFactor;
+    private int n;
+    private int m;
+    private ULLMap<K, V>[] buckets;
+    private HashSet<K> keySet;
+
+
+    public MyHashMap() {
+        this(16);
+    }
+
+    public MyHashMap(int initialSize) {
+        this(initialSize, 0.75);
+    }
+
+    public MyHashMap(int initialSize, double loadFactor) {
+        n = 0;
+        m = initialSize;
+        this.loadFactor = loadFactor;
+        buckets = (ULLMap<K, V>[]) new ULLMap[m];
+        for (int i = 0; i < m; i++) {
+            buckets[i] = new ULLMap<>();
+        }
+        keySet = new HashSet<>();
+    }
+
+    private void resize(int bucketSize) {
+        m = bucketSize;
+        ULLMap<K, V>[] newBuckets = (ULLMap<K, V>[]) new ULLMap[m];
+        for (int i = 0; i < m; i++) {
+            newBuckets[i] = new ULLMap<>();
+        }
+        for (ULLMap<K, V> lst: buckets) {
+            for (K key : lst) {
+                V value = lst.get(key);
+                newBuckets[hash(key)].put(key, value);
+            }
+        }
+        buckets = newBuckets;
+    }
+
+    @Override
+    public void clear() {
+        MyHashMap<K, V> clearMap = new MyHashMap<>();
+        this.m = clearMap.m;
+        this.n = clearMap.n;
+        this.keySet = clearMap.keySet;
+        this.buckets = clearMap.buckets;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return buckets[hash(key)].containsKey(key);
+    }
+
+    @Override
+    public V get(K key) {
+        return buckets[hash(key)].get(key);
+    }
+
+    @Override
+    public int size() {
+        return n;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        buckets[hash(key)].put(key, value);
+        keySet.add(key);
+        n = keySet.size();
+        if ((double)n / m > loadFactor) {
+            resize(m * 2);
+        }
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return keySet;
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet.iterator();
+    }
+
+    private int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+}
