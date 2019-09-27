3ee55739aa4f40753c365b9f9be740c61a687388
j19950805 <j19950805@gmail.com>
2019-09-27 12:02:20 +0800

    Solved flight

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,20 +1,49 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
- * Assumes valid input, i.e. all Flight start times are >= end times.
- * If a flight starts at the same time as a flight's end time, they are
+ * Assumes valid input, i.e. all Flight START times are >= END times.
+ * If a flight STARTs at the same time as a flight's END time, they are
  * considered to be in the air at the same time.
  */
 public class FlightSolver {
+    private static final Comparator<Flight> START_COMPARATOR = (f1, f2) -> f1.startTime - f2.startTime;
+    private static final Comparator<Flight> END_COMPARATOR = (f1, f2) -> f1.endTime - f2.endTime;
+
+    private ArrayList<Flight> flights;
 
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        this.flights = flights;
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        PriorityQueue<Flight> startTimePQ = new PriorityQueue<>(START_COMPARATOR);
+        PriorityQueue<Flight> endTimePQ = new PriorityQueue<>(END_COMPARATOR);
+        for (Flight flight: flights) {
+            startTimePQ.add(flight);
+            endTimePQ.add(flight);
+        }
+        int maxNum = 0;
+        int tally = 0;
+
+        while (!startTimePQ.isEmpty()) {
+            int startTime = startTimePQ.peek().startTime();
+            int endTime = endTimePQ.peek().endTime();
+
+            if (startTime <= endTime) {
+                Flight f = startTimePQ.remove();
+                tally += f.passengers();
+            } else {
+                Flight f = endTimePQ.remove();
+                tally -= f.passengers();
+            }
+
+            maxNum = Math.max(tally, maxNum);
+        }
+        return maxNum;
     }
+
 
 }
