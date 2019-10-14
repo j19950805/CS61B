d2068621d5d032f2aa51276333f51c1ffb4fd044
j19950805 <j19950805@gmail.com>
2019-10-14 13:54:00 +0800

    Completed BnBSolver

--- clab11/BnBSolver.java
+++ clab11/BnBSolver.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,24 +9,69 @@ import java.util.List;
  * size as the ith Bed.
  */
 public class BnBSolver {
+    private List<Bear> bears;
+    private List<Bed> beds;
 
     public BnBSolver(List<Bear> bears, List<Bed> beds) {
-        // TODO: Fix me.
+        this.bears = bears;
+        this.beds = beds;
+        sortBedsWithRandomBear(0, bears.size() - 1);
+    }
+
+    private void sortBedsWithRandomBear(int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        Bear pivot = bears.get(StdRandom.uniform(start, end + 1));
+        int i = start;
+        int j = end;
+        while (i < j) {
+            int cmp1 = pivot.compareTo(beds.get(i));
+            int cmp2 = pivot.compareTo(beds.get(j));
+            if (cmp1 > 0) {
+                i++;
+            }
+            if (cmp2 < 0) {
+                j--;
+            }
+            if (cmp1 <= 0 && cmp2 >= 0) {
+                Collections.swap(beds, i, j);
+            }
+        }
+        sortBearsWithBed(beds.get(i), start, end);
+    }
+
+    private void sortBearsWithBed(Bed pivot, int start, int end) {
+        int i = start;
+        int j = end;
+        while (i < j) {
+            int cmp1 = pivot.compareTo(bears.get(i));
+            int cmp2 = pivot.compareTo(bears.get(j));
+            if (cmp1 > 0) {
+                i++;
+            }
+            if (cmp2 < 0) {
+                j--;
+            }
+            if (cmp1 <= 0 && cmp2 >= 0) {
+                Collections.swap(bears, i, j);
+            }
+        }
+        sortBedsWithRandomBear(start, i - 1);
+        sortBedsWithRandomBear(i + 1, end);
     }
 
     /**
      * Returns List of Bears such that the ith Bear is the same size as the ith Bed of solvedBeds().
      */
     public List<Bear> solvedBears() {
-        // TODO: Fix me.
-        return null;
+        return bears;
     }
 
     /**
      * Returns List of Beds such that the ith Bear is the same size as the ith Bear of solvedBears().
      */
     public List<Bed> solvedBeds() {
-        // TODO: Fix me.
-        return null;
+        return beds;
     }
 }
